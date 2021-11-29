2570526727d44bc33e42d7f9085ab3964973e328
Emmanuel <emmanuelonwukaazu@gmail.com>
2021-11-29 19:54:29 +0100

    updated commit

--- routers/orderRouter.js
+++ routers/orderRouter.js
@@ -1,6 +1,6 @@
 import express from "express";
 import expressAsyncHandler from "express-async-handler";
-import Order from "../../models/orderModels.js.js";
+import Order from "../models/orderModels.js";
 import { isAuth } from "../utils.js";
 
 const orderRouter = express.Router();
